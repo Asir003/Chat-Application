b125ed35231ddb8c4d19a0a091377593415ac9bf
Asir Hamim <161163913+Asir003@users.noreply.github.com>
2025-03-08 23:35:50 +0600

    Update Server.java

--- Server.java
+++ Server.java
@@ -1,22 +1,22 @@
 import java.net.*;
 import java.io.*;
 
-public class Server {
-    public static void main(String[] args) {
-        try {
-            ServerSocket serverSocket = new ServerSocket(12346);
-            System.out.println("Server started. Waiting for clients...");
+public class Server{
+    public static void main(String[] args){
+        try{
+            ServerSocket serverSocket=new ServerSocket(12346);
+            System.out.println("Server started.Waiting for clients...");
 
-            Socket clientSocket = serverSocket.accept();
+            Socket clientSocket=serverSocket.accept();
             System.out.println("Client connected.");
 
-            BufferedReader in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
-            PrintWriter out = new PrintWriter(clientSocket.getOutputStream(), true);
+            BufferedReader in=new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+            PrintWriter out=new PrintWriter(clientSocket.getOutputStream(), true);
 
             String message;
-            while ((message = in.readLine()) != null) {
-                System.out.println("Client: " + message);
-                out.println("Server received: " + message);
+            while ((message=in.readLine())!=null) {
+                System.out.println("Client: "+message);
+                out.println("Server received: "+message);
             }
 
             in.close();
